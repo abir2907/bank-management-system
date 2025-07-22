ab45a798193d9b13b64be8e6761ec442fef54536
ABIR <ABIR@Abir>
2025-07-22 09:37:21 +0530

    Create SignupOne class for first page of signup

---
+++ src/bank/management/system/SignupOne.java
@@ -0,0 +1,19 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SignupOne extends JFrame {
+    
+    SignupOne() {        
+        getContentPane().setBackground(Color.WHITE);
+        
+        setSize(850, 800);
+        setLocation(350, 10);
+        setVisible(true);
+    }
+    
+    public static void main(String args[]) {
+        new SignupOne();
+    }
+}
